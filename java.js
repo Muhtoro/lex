bf00429d5681bf3af2cb0b1dad0c4f9f1ad3f8de
Muhtoro <[200~a42440440@gmail.com>
2024-04-06 11:25:20 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,13 @@
+
+
+
+let randomnum=(Math.floor(Math.random() * 100))
+
+
+
+alert( `random number: `+randomnum)
+
+
+alert(randomnum+` agar ikkiga kopaytirsak `+randomnum*2 +`ga teng`)
+
+alert(randomnum+` ni ikkaga bolsak `+ randomnum/2 + `ga teng`+ ` va uning qoldiq ` + randomnum%2)
